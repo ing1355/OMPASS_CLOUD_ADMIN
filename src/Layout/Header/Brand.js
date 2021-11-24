8622c68f8792f1d8590d9935eb0966c50a7ac6a3
kimjiho <hozzi@omsecurity.kr>
2021-11-24 13:36:20 +0900

    Brand Dashboard 이동 추가

--- src/Layout/Header/Brand.js
+++ src/Layout/Header/Brand.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Brand.css";
 import logo from "../../assets/logo.png";
+import { useHistory } from "react-router";
 const Brand = () => {
+  const history = useHistory();
   return (
-    <div className="brand" style={{ fontWeight: "bold", fontSize: "1.5rem" }}>
-      {/* <p> OMPASS</p> */}
+    <div className="brand" style={{ fontWeight: "bold", fontSize: "1.5rem", cursor:'pointer' }} onClick={() => {
+      history.push('/')
+    }}>
       <img src={logo} />
     </div>
   );
