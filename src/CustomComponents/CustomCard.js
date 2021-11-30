c902f977410750967dc320c0549d189e96c82492
kimjiho <hozzi@omsecurity.kr>
2021-11-30 16:45:39 +0900

    OMS Dashboard 작업중

---
+++ src/CustomComponents/CustomCard.js
@@ -0,0 +1,22 @@
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './CustomCard.css'
+
+const CustomCard = ({ title, children }) => {
+    return <div className="custom-card-container">
+        <div className="custom-card-icon-container">
+            <FontAwesomeIcon icon={faUser} size="4x" />
+        </div>
+        <div className="custom-card-contents-container">
+            <div className="custom-card-content-container">
+                {children}
+            </div>
+            <div className="custom-card-title-container">
+                {title}
+            </div>
+        </div>
+    </div>
+}
+
+export default CustomCard;
