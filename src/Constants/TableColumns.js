a7893284c9c6a89af61196b0b9e76e94855c3954
최유림 <dbflagovl12@omsecurity.kr>
2021-12-21 15:29:53 +0900

    다국어 수정

--- src/Constants/TableColumns.js
+++ src/Constants/TableColumns.js
@@ -4,11 +4,11 @@ import { slicePrice } from "../Functions/SlicePrice";
 import { FormattedMessage } from "react-intl";
 
 export const AdminsColumns = [
-  { name: <FormattedMessage id="Name" />, key: "name", width: 220 },
-  { name: <FormattedMessage id="Email" />, key: "email", width: 250 },
-  { name: <FormattedMessage id="Authority" />, key: "role" },
-  { name: <FormattedMessage id="phoneNumber" />, key: "phone", width: 250 },
-  { name: <FormattedMessage id="Country" />, key: "country" },
+  { name: "Name", key: "name", width: 220 },
+  { name: "Email", key: "email", width: 250 },
+  { name: "Authority", key: "role" },
+  { name: "phoneNumber", key: "phone", width: 250 },
+  { name: "Country", key: "country" },
 ];
 
 const makeDetail = (data, row) => (
@@ -49,63 +49,56 @@ export const BillingColumns = [
 ];
 
 export const DashboardLogColumns = [
-  { name: <FormattedMessage id="User" />, key: "userId" },
-  { name: <FormattedMessage id="Action" />, key: "act" },
-  { name: <FormattedMessage id="Application" />, key: "appName" },
-  { name: <FormattedMessage id="Status" />, key: "status" },
-  { name: <FormattedMessage id="Date" />, key: "createdDate" },
+  { name: "User", key: "userId" },
+  { name: "Action", key: "act" },
+  { name: "Application", key: "appName" },
+  { name: "Status", key: "status" },
+  { name: "Date", key: "createdDate" },
 ];
 
 export const LogsColumns = [
   {
-    name: <FormattedMessage id="User" />,
-    title: "User",
+    name: "User",
     key: "userId",
     width: 250,
     searched: true,
   },
   {
-    name: <FormattedMessage id="Action" />,
-    title: "활동",
+    name: "Action",
     key: "act",
     searched: true,
     searchedOptions: ["register", "authenticate"],
   },
   {
-    name: <FormattedMessage id="Application" />,
-    title: "어플리케이션",
+    name: "Application",
     key: "appName",
     searched: true,
     width: 250,
   },
   {
-    name: <FormattedMessage id="Status" />,
-    title: "상태",
+    name: "Status",
     key: "status",
     searched: true,
     searchedOptions: ["success", "fail"],
   },
-  { name: <FormattedMessage id="Date" />, key: "createdDate", width: 250 },
+  { name: "Date", key: "createdDate", width: 250 },
 ];
 
 export const allUserColumns = [
   {
-    name: <FormattedMessage id="Users" />,
-    title: "사용자 아이디",
+    name: "Users",
     key: "userId",
     width: 250,
     searched: true,
   },
   {
-    name: <FormattedMessage id="Application" />,
-    title: "어플리케이션",
+    name: "Application",
     key: "appName",
     width: 250,
     searched: true,
   },
   {
-    name: <FormattedMessage id="Status" />,
-    title: "상태",
+    name: "Status",
     key: "type",
     searched: true,
     searchedOptions: ["ompass"],
@@ -116,8 +109,7 @@ export const allUserColumns = [
     width: 250,
   },
   {
-    name: <FormattedMessage id="Bypass" />,
-    title: "2차인증 바이패스",
+    name: "Bypass",
     key: "byPass",
     render: (byPass) => {
       return <CustomSwitch defaultChecked={byPass} />;
@@ -130,14 +122,14 @@ export const allUserColumns = [
 ];
 
 export const disabledUserColumns = [
-  { name: "아이디", key: "userId", width: 200, searched: true },
+  { name: "Users", key: "userId", width: 200, searched: true },
   {
-    name: <FormattedMessage id="Application" />,
+    name: "Application",
     key: "appName",
     searched: true,
   },
   {
-    name: "상태",
+    name: "Status",
     key: "type",
     width: 200,
     searched: true,
@@ -158,10 +150,10 @@ export const disabledUserColumns = [
 ];
 
 export const byPassUserColumns = [
-  { name: "아이디", key: "userId", width: 200, searched: true },
-  { name: "어플리케이션", key: "appName", searched: true },
+  { name: "Users", key: "userId", width: 200, searched: true },
+  { name: "Application", key: "appName", searched: true },
   {
-    name: "상태",
+    name: "Status",
     key: "type",
     width: 200,
     searched: true,
