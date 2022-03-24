9bea2539524e9968edf12c1857446edbd59b69d2
최유림 <dbflagovl12@omsecurity.kr>
2022-03-24 18:28:45 +0900

    admin info 테이블 css

--- src/Layout/OMSRole/Admins/AdminInfo.js
+++ src/Layout/OMSRole/Admins/AdminInfo.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import './AdminInfo.css'
+import React from "react";
+import "./AdminInfo.css";
 
 const AdminInfo = ({ data }) => {
-    const { email, country, company, name, phone } = data || {};
-    return <section>
-        <h2>User Info</h2>
-        <div>
-            Email : {email}
-        </div>
-        <div>
-            Company : {company}
-        </div>
-        <div>
-            Country : {country}
-        </div>
-        <div>
-            Name : {name}
-        </div>
-        <div>
-            Phone : {phone}
-        </div>        
+  const { email, country, company, name, phone } = data || {};
+  return (
+    <section className="admin-info-section" style={{ border: "0" }}>
+      <h2>User Info</h2>
+      <div>
+        Email : <b>{email}</b>
+      </div>
+      <div>
+        Company : <b>{company}</b>
+      </div>
+      <div>
+        Country : <b>{country}</b>
+      </div>
+      <div>
+        Name : <b>{name}</b>
+      </div>
+      <div>
+        Phone : <b>{phone}</b>
+      </div>
     </section>
-}
+  );
+};
 
-export default AdminInfo
+export default AdminInfo;
