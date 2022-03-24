de5e355ffcb5aad07ed65c739248cc53b5c97014
최유림 <dbflagovl12@omsecurity.kr>
2022-03-24 15:49:35 +0900

    oms계정 상단 css

--- src/Layout/OMSRole/AppManagement/Android.js
+++ src/Layout/OMSRole/AppManagement/Android.js
@@ -1,79 +1,120 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
-import { getAppManagementApi, registerAppManagementApi, updateAppManagementApi } from '../../../Constants/Api_Route';
-import { AppManagementAndroidColumns } from '../../../Constants/TableColumns';
-import CustomTable from '../../../CustomComponents/CustomTable';
-import { CustomAxiosGet, CustomAxiosPost, CustomAxiosPut } from '../../../Functions/CustomAxios';
+import React, { useLayoutEffect, useRef, useState } from "react";
+import {
+  getAppManagementApi,
+  registerAppManagementApi,
+  updateAppManagementApi,
+} from "../../../Constants/Api_Route";
+import { AppManagementAndroidColumns } from "../../../Constants/TableColumns";
+import CustomTable from "../../../CustomComponents/CustomTable";
+import {
+  CustomAxiosGet,
+  CustomAxiosPost,
+  CustomAxiosPut,
+} from "../../../Functions/CustomAxios";
 
 const Android = () => {
-    const [appVersion, setAppVersion] = useState(null);
-    const [tableData, setTableData] = useState([])
-    const [tableLoading, setTableLoading] = useState(true);
-    const fileRef = useRef(null);
+  const [appVersion, setAppVersion] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [tableLoading, setTableLoading] = useState(true);
+  const fileRef = useRef(null);
 
-    useLayoutEffect(() => {
-        CustomAxiosGet(getAppManagementApi, data => {
-            console.log(data)
-            setTableLoading(false);
-            setTableData(data)
-        }, err => {
-            setTableLoading(false);
-        }, {
-            osType: 'android'
-        })
-    }, [])
+  useLayoutEffect(() => {
+    CustomAxiosGet(
+      getAppManagementApi,
+      (data) => {
+        console.log(data);
+        setTableLoading(false);
+        setTableData(data);
+      },
+      (err) => {
+        setTableLoading(false);
+      },
+      {
+        osType: "android",
+      }
+    );
+  }, []);
 
-    const onChangeAppVersion = (e) => {
-        setAppVersion(e.target.value);
-    }
+  const onChangeAppVersion = (e) => {
+    setAppVersion(e.target.value);
+  };
 
-    const uploadApk = (e) => {
-        const formData = new FormData();
-        formData.append('file', fileRef.current.files[0])
-        formData.append('version', appVersion)
-        formData.append('os', 'android')
-        setTableLoading(true);
-        if (tableData.find(t => t.version === appVersion)) {
-            CustomAxiosPut(updateAppManagementApi, formData, res => {
-                console.log(res);
-                setTableLoading(false);
-            }, err => {
-                console.log(err)
-                setTableLoading(false);
-            }, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
-        } else {
-            CustomAxiosPost(registerAppManagementApi, formData, res => {
-                console.log(res);
-                setTableLoading(false);
-            }, err => {
-                console.log(err)
-                setTableLoading(false);
-            }, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
+  const uploadApk = (e) => {
+    const formData = new FormData();
+    formData.append("file", fileRef.current.files[0]);
+    formData.append("version", appVersion);
+    formData.append("os", "android");
+    setTableLoading(true);
+    if (tableData.find((t) => t.version === appVersion)) {
+      CustomAxiosPut(
+        updateAppManagementApi,
+        formData,
+        (res) => {
+          console.log(res);
+          setTableLoading(false);
+        },
+        (err) => {
+          console.log(err);
+          setTableLoading(false);
+        },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
+      );
+    } else {
+      CustomAxiosPost(
+        registerAppManagementApi,
+        formData,
+        (res) => {
+          console.log(res);
+          setTableLoading(false);
+        },
+        (err) => {
+          console.log(err);
+          setTableLoading(false);
+        },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
     }
-    return <div className="contents-container">
-        <div>
-            앱 버전 : <input value={appVersion || ""} onChange={onChangeAppVersion} />
+  };
+  return (
+    <div className="contents-container">
+      <div className="app-management-div">
+        <div style={{ marginBottom: "1rem" }}>
+          앱 버전 :{" "}
+          <input value={appVersion || ""} onChange={onChangeAppVersion} />
         </div>
-        <div>
-            앱 무결성 체크 : <input type="file" name="file" ref={fileRef} id="file" accept=".apk" />
-            <button onClick={uploadApk}>업로드</button>
-        </div>
-        <CustomTable
-            columns={AppManagementAndroidColumns}
-            datas={tableData}
-            pagination
-            rowClick={({ version }) => { setAppVersion(version) }}
-            numPerPage={10}
-            loading={tableLoading} />
-    </div>
-}
 
-export default Android
+        <div>
+          앱 무결성 체크 :{" "}
+          <input
+            type="file"
+            name="file"
+            ref={fileRef}
+            id="file"
+            accept=".apk"
+          />
+          <button onClick={uploadApk}>업로드</button>
+        </div>
+      </div>
+      <CustomTable
+        columns={AppManagementAndroidColumns}
+        datas={tableData}
+        pagination
+        rowClick={({ version }) => {
+          setAppVersion(version);
+        }}
+        numPerPage={10}
+        loading={tableLoading}
+      />
+    </div>
+  );
+};
+
+export default Android;
--- src/Layout/OMSRole/AppManagement/IOS.js
+++ src/Layout/OMSRole/AppManagement/IOS.js
@@ -1,59 +1,82 @@
-import React, { useLayoutEffect, useState } from 'react'
-import { registerAppManagementApi } from '../../../Constants/Api_Route';
-import { AppManagementIOSColumns } from '../../../Constants/TableColumns';
-import CustomTable from '../../../CustomComponents/CustomTable';
-import { CustomAxiosGet, CustomAxiosPost } from '../../../Functions/CustomAxios';
+import React, { useLayoutEffect, useState } from "react";
+import { registerAppManagementApi } from "../../../Constants/Api_Route";
+import { AppManagementIOSColumns } from "../../../Constants/TableColumns";
+import CustomTable from "../../../CustomComponents/CustomTable";
+import {
+  CustomAxiosGet,
+  CustomAxiosPost,
+} from "../../../Functions/CustomAxios";
 
 const IOS = () => {
-    const [appVersion, setAppVersion] = useState(null);
-    const [tableData, setTableData] = useState([])
-    const [tableLoading, setTableLoading] = useState(true);
+  const [appVersion, setAppVersion] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [tableLoading, setTableLoading] = useState(true);
 
-    useLayoutEffect(() => {
-        CustomAxiosGet('/oms/app/management', data => {
-            console.log(data)
-            setTableLoading(false);
-            setTableData(data)
-        }, err => {
-            setTableLoading(false);
-        }, {
-            osType: 'ios'
-        })
-    }, [])
+  useLayoutEffect(() => {
+    CustomAxiosGet(
+      "/oms/app/management",
+      (data) => {
+        console.log(data);
+        setTableLoading(false);
+        setTableData(data);
+      },
+      (err) => {
+        setTableLoading(false);
+      },
+      {
+        osType: "ios",
+      }
+    );
+  }, []);
 
-    const onChangeAppVersion = (e) => {
-        setAppVersion(e.target.value);
-    }
+  const onChangeAppVersion = (e) => {
+    setAppVersion(e.target.value);
+  };
 
-    const upadteIosVersion = () => {
-        const formData = new FormData();
-        formData.append('version', appVersion)
-        formData.append('os', 'ios')
-        CustomAxiosPost(registerAppManagementApi, formData, res => {
-            console.log(res);
-            setTableLoading(false);
-        }, err => {
-            console.log(err)
-            setTableLoading(false);
-        }, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
-    }
-    return <div className="contents-container">
+  const upadteIosVersion = () => {
+    const formData = new FormData();
+    formData.append("version", appVersion);
+    formData.append("os", "ios");
+    CustomAxiosPost(
+      registerAppManagementApi,
+      formData,
+      (res) => {
+        console.log(res);
+        setTableLoading(false);
+      },
+      (err) => {
+        console.log(err);
+        setTableLoading(false);
+      },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+  };
+  return (
+    <div className="contents-container">
+      <div className="app-management-div">
         <div>
-            앱 버전 : <input value={appVersion || ""} onChange={onChangeAppVersion} />
+          앱 버전 :{" "}
+          <input value={appVersion || ""} onChange={onChangeAppVersion} />
+          <button onClick={upadteIosVersion}>변경</button>
         </div>
-        <button onClick={upadteIosVersion}>변경</button>
-        <CustomTable
-            columns={AppManagementIOSColumns}
-            datas={tableData}
-            pagination
-            rowClick={({ version }) => { setAppVersion(version) }}
-            numPerPage={10}
-            loading={tableLoading} />
-    </div>
-}
+      </div>
 
-export default IOS
+      <CustomTable
+        columns={AppManagementIOSColumns}
+        datas={tableData}
+        pagination
+        rowClick={({ version }) => {
+          setAppVersion(version);
+        }}
+        numPerPage={10}
+        loading={tableLoading}
+      />
+    </div>
+  );
+};
+
+export default IOS;
